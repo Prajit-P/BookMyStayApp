62e83b3da073f08abfc5bb4a5f6fad59e40e4ec5
Prajit-P <prajit2577@gmail.com>
2026-03-12 09:08:48 +0530

    Use Case 1: Application Entry & Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // Inform user the application has started successfully
+        System.out.println("System initialized successfully.");
+
     }
 }
